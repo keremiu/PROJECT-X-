6698b4ea4cb0c5e52506d9ba327c5668e40c4e95
OmerFarukMerey <78052303+OmerFarukMerey@users.noreply.github.com>
2024-01-26 16:09:35 +0300

    search suggestion bar starter code

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
+
+var categories = require("./data/categories.json");
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    console.log("search ", searchTerm);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Category</h1>
+      <div className="search-container">
+        <div className="search-inner">
+          <input type="text" value={value} onChange={onChange}></input>
+
+        </div>
+        <div className='dropdown'>
+          {categories.filter(item => {
+            const searchTerm = value.toLowerCase();
+            const cat = item.key.toLowerCase();
+
+            return searchTerm && cat.includes(searchTerm) && searchTerm !== cat
+          }).map((item) => (
+          <div onClick={() => onSearch(item.key)}
+          className='dropdown-row' 
+          key={item.key}> 
+          {item.key}</div>
+          ))} 
+        </div>
+      </div>
     </div>
   );
 }
